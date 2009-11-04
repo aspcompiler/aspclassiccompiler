7a719ebac9063c545e29ac86c3a1926672748fcb
dotneteer <none@none>
2009-11-04 05:54:37 +0000

    Silverlight sample using VBScript.net compiler
    --HG--
    extra : convert_revision : svn%3Aa83551a4-30f6-4d81-a974-c6ced450ddbf%4031328

---
+++ aspclassiccompiler/SilverlightVBScriptSample/SilverlightApplication1/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Scripting.Hosting;
+using Dlrsoft.VBScript.Hosting;
+
+namespace SilverlightApplication1
+{
+    public partial class MainPage : UserControl
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void MyButton_Click(object sender, RoutedEventArgs e)
+        {
+            VbscriptHost vbscript = ((App)Application.Current).VBScript;
+            ScriptScope scope = vbscript.CreateScope();
+            scope.SetVariable("page", this);
+            //this.MyButton.Content = "Clicked";
+            string code = "page.MyButton.Content = \"Clicked\"";
+            CompiledCode compiled = vbscript.Compile(code);
+            compiled.Execute(scope);
+        }
+    }
+}
---
+++ aspclassiccompiler/SilverlightVBScriptSample/SilverlightApplication1/VbscriptHost.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Scripting;
+using Microsoft.Scripting.Hosting;
+using Microsoft.Scripting.Silverlight;
+using Dlrsoft.VBScript.Hosting;
+
+namespace SilverlightApplication1
+{
+    public class VbscriptHost
+    {
+        private ScriptRuntime _runtime = null;
+        private ScriptEngine _engine;
+
+        public VbscriptHost()
+        {
+            var setup = new ScriptRuntimeSetup();
+            string qualifiedname = typeof(VBScriptContext).AssemblyQualifiedName;
+            setup.LanguageSetups.Add(new LanguageSetup(
+                qualifiedname, "vbscript", new[] { "vbscript" }, new[] { ".vbs" }));
+            setup.HostType = typeof(BrowserScriptHost);
+            _runtime = new ScriptRuntime(setup);
+            _engine = _runtime.GetEngine("vbscript");
+
+            _runtime.LoadAssembly(typeof(VBScriptContext).Assembly);
+        }
+
+        public ScriptScope CreateScope()
+        {
+            return _engine.CreateScope();
+        }
+
+        public CompiledCode Compile(string code)
+        {
+            ScriptSource src = _engine.CreateScriptSourceFromString(code, SourceCodeKind.File);
+            return src.Compile();
+        }
+    }
+}
